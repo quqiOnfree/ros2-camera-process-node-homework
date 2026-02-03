53999ba0f76a971a44d559e7963ce2bd536bcc4f
quqiOnfree <quqiOnfree@outlook.com>
2026-02-03 18:43:00 +0800

    引入usb_cam与OpenCV库进入detect

--- src/detect/src/detect_node.cpp
+++ src/detect/src/detect_node.cpp
@@ -1,6 +1,10 @@
 #include <cstdio>
 #include <rclcpp/node.hpp>
 #include <std_msgs/msg/string.h>
+#include <usb_cam/formats/utils.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 
 class DetectNode: rclcpp::Node {
 public:
@@ -11,5 +15,7 @@ public:
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char ** argv)
 {
+  // cv::Mat img;
+
   printf("hello world detect package\n");
 }
